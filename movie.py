f869f4facd703ea7be9bcd4f6e8d23883bea7690
ChanPyaeAung11 <chanpyaeaung@my.jcu.edu.au>
2020-01-05 16:00:27 +0800

    add two functions that return True if a movie is watched or not

--- movie.py
+++ movie.py
@@ -17,3 +17,11 @@ class Movie:
     def __str__(self):
         """ return strings about movie details"""
         return "{}, {}, {}, {}".format(self.name, self.year, self.category, self.watched)
+
+    def check_watched(self):
+        """ return True if a movie is watched"""
+        return self.watched == "w"
+
+    def check_unwatched(self):
+        """ return True if a movie is unwatched"""
+        return self.watched == "u"
