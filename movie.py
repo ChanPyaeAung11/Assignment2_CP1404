6c3d87dc9c82c75960bcb38458bfdcb7d7234a85
ChanPyaeAung11 <chanpyaeaung@my.jcu.edu.au>
2020-01-05 15:58:21 +0800

    filled out the details

--- movie.py
+++ movie.py
@@ -1,9 +1,10 @@
-"""..."""
-
-
-# TODO: Create your Movie class in this file
+"""
+Name: Chan Pyae Aung
+Date Started: 5 Jan 2020
+GitHub URL: https://github.com/JCUS-CP1404/assignment-02-ChanPyaeAung11
+"""
 
 
 class Movie:
-    """..."""
-    pass
+
+
