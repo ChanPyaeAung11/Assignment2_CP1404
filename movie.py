42fb4fe6beb7879780af449b1597a8cea53c8862
ChanPyaeAung11 <chanpyaeaung@my.jcu.edu.au>
2020-01-15 09:36:12 +0800

    change the naming according to test_movie.py

--- movie.py
+++ movie.py
@@ -7,21 +7,21 @@ GitHub URL: https://github.com/JCUS-CP1404/assignment-02-ChanPyaeAung11
 
 class Movie:
 
-    def __init__(self, name, year, cateogry, watched):
-        """ use parameters pass =ed in this and construct them"""
-        self.name = name
+    def __init__(self, title="", year=0, category="", is_watched=""):
+        """ use parameters passed in this and construct them"""
+        self.title = title
         self.year = year
-        self.category = cateogry
-        self.watched = watched
+        self.category = category
+        self.is_watched = is_watched
 
     def __str__(self):
         """ return strings about movie details"""
-        return "{}, {}, {}, {}".format(self.name, self.year, self.category, self.watched)
+        return "{}, {}, {}, Watched = {}".format(self.title, self.year, self.category, self.is_watched)
 
     def check_watched(self):
         """ return True if a movie is watched"""
-        return self.watched == "w"
+        return self.is_watched == "w"
 
     def check_unwatched(self):
         """ return True if a movie is unwatched"""
-        return self.watched == "u"
+        return self.is_watched == "u"
