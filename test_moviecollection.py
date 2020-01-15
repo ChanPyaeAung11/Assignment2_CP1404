4fa3b268dbe72b91ccf4c4ef6f73ebea1c7c0464
ChanPyaeAung11 <chanpyaeaung@my.jcu.edu.au>
2020-01-15 11:06:29 +0800

    test out methods from moviecollection.py

--- test_moviecollection.py
+++ test_moviecollection.py
@@ -20,13 +20,24 @@ def run_tests():
 
     # Test adding a new Movie with values
     print("Test adding new movie:")
-    movie_collection.add_movie(Movie("Amazing Grace", 2006, "Drama", False))
+    new_movie = []
+    title = input("Movie title: ")
+    new_movie.append(title)
+    year = input("Year Released: ")
+    new_movie.append(year)
+    category = input("Genre: ")
+    new_movie.append(category)
+    new_movie.append("u")
+    print(new_movie)
+    movie_collection.add_movie(new_movie)
     print(movie_collection)
 
     # Test sorting movies
+    """
     print("Test sorting - year:")
     movie_collection.sort("year")
     print(movie_collection)
+    """
     # TODO: Add more sorting tests
 
     # TODO: Test saving movies (check CSV file manually to see results)
