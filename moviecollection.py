becc50d366abf6062f65b5237455bf5d26005465
ChanPyaeAung11 <chanpyaeaung@my.jcu.edu.au>
2020-01-15 10:21:02 +0800

    __init__ function, __str__ function and load_movies are written

--- moviecollection.py
+++ moviecollection.py
@@ -6,4 +6,26 @@ GitHub URL: https://github.com/JCUS-CP1404/assignment-02-ChanPyaeAung11
 
 
 class MovieCollection:
+
+    def __init__(self, movies=None):
+        """ construct things needed for the class"""
+        if movies is None:
+            movies = []
+        self.movies = movies
+
+    def __str__(self):
+        """ print out the movies list"""
+        return "Movies :{}".format(self.movies)
+
+    def load_movies(self, csv):
+        """ load movies from the file and load them into the movies list """
+        load_file = open(csv, "r")
+        for line in load_file:
+            if not line.strip():
+                continue
+            else:
+                self.movies.append(line)
+        self.movies = [line.strip('\n') for line in self.movies]
+        self.movies = [line.split(",") for line in self.movies]
+
     pass
