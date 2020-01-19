14bb30f702d966f71bc0ec7014eb3463eb063f4a
ChanPyaeAung11 <chanpyaeaung@my.jcu.edu.au>
2020-01-20 00:33:14 +0800

    make functions simpler to use in main py testing is not complete but these functions are usable in main

--- moviecollection.py
+++ moviecollection.py
@@ -5,31 +5,22 @@ GitHub URL: https://github.com/JCUS-CP1404/assignment-02-ChanPyaeAung11
 """
 from movie import Movie
 
+
 class MovieCollection:
 
+    def __str__(self):  # for testing
+        """ print out the movies list"""
+        return "Movies :{}".format(self.movies)
+
     def __init__(self):
-        """ construct things needed for the class"""
+        """ construct things needed for the class and initiate watched and unwatched movies"""
         self.movies = []
         self.unwatch_movie = ''
         self.watch_movie = ''
 
-    def __str__(self):
-        """ print out the movies list"""
-        return "Movies :{}".format(self.movies)
-
-    def load_movies(self, csv):
-        """ load movies from the file and load them into the movies list """
-        with open(csv, 'r') as csv:
-            for line in csv.readlines():
-                title, year, category, is_watched = line.replace("\n", "").split(",")
-                is_watched = is_watched == "w"
-                i = Movie(title, year, category, is_watched)
-                self.movies.append(i)
-
     def add_movie(self, new_movie):
         """ function to add new movies to the movies list"""
         self.movies.append(new_movie)
-        return self.movies
 
     def get_unwatched_movies(self):
         """ function to get numbers of unwatched movies"""
@@ -38,18 +29,25 @@ class MovieCollection:
         self.unwatch_movie = left
 
     def get_watched_movies(self):
-        """ function to get numbers of watched movies"""
         self.watch_movie = ''
         right = len([movie for movie in self.movies if movie.is_watched])
         self.watch_movie = right
 
-    def sort_movies(self, sort_by):
-        """ sort movies according to given types"""
-        self.movies.sort(key=lambda x: getattr(x, sort_by))
+    def load_movies(self, file):
+        """ load movies from the file and load them into the movies list """
+        with open(file, 'r') as csv:
+            for line in csv.readlines():
+                title, year, category, is_watched = line.replace("\n", "").split(",")
+                is_watched = is_watched == "w"
+                i = Movie(title, year, category, is_watched)
+                self.movies.append(i)  # add the movie data into the list
 
-    def save_file(self, csv):
-        """ saves the final list into the file """
-        with open(csv, 'w'):
+    def save_file(self, output_file="movies.csv"):
+        """to write the list of data into the csv file"""
+        with open(output_file, 'w') as csv:
             for movie in self.movies:
-                csv.write(movie.save_csv())
+                csv.write(movie.save_movie())
 
+    def sort_movie(self, sort_by):
+        """to sort the movie based on given types"""
+        self.movies.sort(key=lambda i: getattr(i, sort_by))
--- test_moviecollection.py
+++ test_moviecollection.py
@@ -16,41 +16,31 @@ def run_tests():
     print("Test loading movies:")
     movie_collection.load_movies('movies.csv')
     print(movie_collection)
-    assert movie_collection.movies  # assuming CSV file is non-empty, non-empty list is considered True
+    assert movie_collection.movies
 
     # Test adding a new Movie with values
     print("Test adding new movie:")
-    test_new_movie = ["Cats", 2019, "Musical", "u"]
-    assert test_new_movie[0] == "Cats"
-    assert test_new_movie[1] == 2019
-    assert test_new_movie[2] == "Musical"
-    assert test_new_movie[3] == "u"
-    movie_collection.add_movie(test_new_movie)
+    movie_collection.add_movie(Movie("Cats", 2019, "Musical", "u"))
     print(movie_collection)
 
     # Test sorting movies
 
     print("Test sorting - year:")
-    movie_collection.sort_movies("year")
+    movie_collection.sort_movie("title")
     print(movie_collection)
-    # TODO: Add more sorting tests
-
-    print("Test saving movies into the file")
-    movie_collection.save_file('movies.csv')
 
     # Test to get number of unwatched movies
     print("Test getting number of unwatched movies:")
     unwatched_movies = movie_collection.get_unwatched_movies()
-    assert not unwatched_movies < 0
-    assert unwatched_movies >= 0
     print(unwatched_movies)
 
     # Test to get number of watched movies
     print("Test getting number of watched movies:")
     watched_movies = movie_collection.get_watched_movies()
-    assert not watched_movies < 0
-    assert watched_movies <= 0
     print(watched_movies)
+
+    print("Test saving movies into the file")
+    movie_collection.save_file('movies.csv')
 
 
 run_tests()
